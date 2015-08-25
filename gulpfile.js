9487edd4f228b091850960811fbab0392e359321
Daniel Yates <dyates@thoughtworks.com>
2015-08-25 14:44:03 +0100

    Rename destination folder for project files

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,16 @@ var imagemin = require('gulp-imagemin');
 var jade = require('gulp-jade');
 
 gulp.task('jade', function () {
-  return gulp.src('app/views/**/*.jade')
+  return gulp.src('app/jade/**/*.jade')
     .pipe(jade())
-    .pipe(gulp.dest('src'));
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('styles', function () {
   return gulp.src('app/css/**/*.scss')
     .pipe(concat('application.css'))
     .pipe(sass())
-    .pipe(gulp.dest('src/css'));
+    .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('scripts', function () {
@@ -25,20 +25,20 @@ gulp.task('scripts', function () {
     .pipe(jshint.reporter('default'))
     .pipe(concat('all.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('src/js'));
+    .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('images', function () {
   return gulp.src('app/img/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('src/img'));
+    .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('app/views/**/*.jade')
+  gulp.watch('app/jade/**/*.jade')
   gulp.watch('app/css/**/*.scss')
   gulp.watch('app/js/**/*.js')
   gulp.watch('app/img/**/*')
 });
 
-gulp.task('default', gulp.parallel('jade', 'styles', 'scripts', 'images'));
+gulp.task('default', ['jade', 'styles', 'scripts', 'images', 'watch']);
