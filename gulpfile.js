f4bfd1e42e395ddc13facc5d0b61d5e9325c4b3b
Daniel Yates <dyates@thoughtworks.com>
2015-08-25 15:39:42 +0100

    remove unneeded javascript literal

--- gulpfile.js
+++ gulpfile.js
@@ -7,29 +7,27 @@ var imagemin = require('gulp-imagemin');
 var jade = require('gulp-jade');
 
 var paths = {
-  location: {
-    jade: "app/jade/**/*.jade",
-    styles: "app/css/**/*.scss",
-    scripts: "app/js/**/*.js",
-    images: "app/img/*"
-  }
+  jade: "app/jade/**/*.jade",
+  styles: "app/css/**/*.scss",
+  scripts: "app/js/**/*.js",
+  images: "app/img/*"
 };
 
 gulp.task('jade', function () {
-  return gulp.src(paths.location.jade)
+  return gulp.src(paths.jade)
     .pipe(jade())
     .pipe(gulp.dest('build'));
 });
 
 gulp.task('styles', function () {
-  return gulp.src(paths.location.styles)
+  return gulp.src(paths.styles)
     .pipe(concat('application.css'))
     .pipe(sass())
     .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('scripts', function () {
-  return gulp.src(paths.location.scripts)
+  return gulp.src(paths.scripts)
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(concat('main.js'))
@@ -38,16 +36,16 @@ gulp.task('scripts', function () {
 });
 
 gulp.task('images', function () {
-  return gulp.src(paths.location.images)
+  return gulp.src(paths.images)
     .pipe(imagemin())
     .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(paths.location.jade, ['jade']);
-  gulp.watch(paths.location.styles, ['styles']);
-  gulp.watch(paths.location.scripts, ['scripts']);
-  gulp.watch(paths.location.images, ['images']);
+  gulp.watch(paths.jade, ['jade']);
+  gulp.watch(paths.styles, ['styles']);
+  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.images, ['images']);
 });
 
 gulp.task('default', ['watch', 'jade', 'styles', 'scripts', 'images']);
