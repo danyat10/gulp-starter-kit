7a3763a275097f50a34029926e1ea82aaf4fd660
Daniel Yates <dyates@thoughtworks.com>
2015-08-25 15:09:15 +0100

    Make gulp watch work + Refactor

--- gulpfile.js
+++ gulpfile.js
@@ -6,39 +6,54 @@ var jshint = require('gulp-jshint');
 var imagemin = require('gulp-imagemin');
 var jade = require('gulp-jade');
 
+var paths = {
+  location: {
+    jade: "app/jade/**/*.jade",
+    styles: "app/css/**/*.scss",
+    scripts: "app/js/**/*.js",
+    images: "app/img/*"
+  },
+  destination: {
+    jade: "build",
+    styles: "build/css",
+    scripts: "build/scripts",
+    images: "build/images"
+  }
+};
+
 gulp.task('jade', function () {
-  return gulp.src('app/jade/**/*.jade')
+  return gulp.src(paths.location.jade)
     .pipe(jade())
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest(paths.destination.jade));
 });
 
 gulp.task('styles', function () {
-  return gulp.src('app/css/**/*.scss')
+  return gulp.src(paths.location.styles)
     .pipe(concat('application.css'))
     .pipe(sass())
-    .pipe(gulp.dest('build/css'));
+    .pipe(gulp.dest(paths.destination.styles));
 });
 
 gulp.task('scripts', function () {
-  return gulp.src('app/js/**/*.js')
+  return gulp.src(paths.location.scripts)
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(concat('all.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('build/js'));
+    .pipe(gulp.dest(paths.destination.scripts));
 });
 
 gulp.task('images', function () {
-  return gulp.src('app/img/**/*')
+  return gulp.src(paths.location.images)
     .pipe(imagemin())
-    .pipe(gulp.dest('build/img'));
+    .pipe(gulp.dest(paths.destination.images));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('app/jade/**/*.jade')
-  gulp.watch('app/css/**/*.scss')
-  gulp.watch('app/js/**/*.js')
-  gulp.watch('app/img/**/*')
+  gulp.watch(paths.location.jade, ['jade']);
+  gulp.watch(paths.location.styles, ['styles']);
+  gulp.watch(paths.location.scripts, ['scripts']);
+  gulp.watch(paths.location.images, ['images']);
 });
 
-gulp.task('default', ['jade', 'styles', 'scripts', 'images', 'watch']);
+gulp.task('default', ['watch', 'jade', 'styles', 'scripts', 'images']);
